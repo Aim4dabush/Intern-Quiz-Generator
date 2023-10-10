6feb6c35a62919a342f834169ce794bcf22b855e
Kimberly Encinas <kimbencinas@gmail.com>
2023-10-09 19:08:01 -0700

    added quiz categories

--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -1,8 +1,40 @@
+import React, { useState, useEffect } from 'react';
 import "./HomePage.css";
 
 function HomePage() {
+  const [quizData, setQuizData] = useState('');
+  const [quizCategory, setQuizCategory] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://the-trivia-api.com/v2/questions/`)
+      .then((response) => response.json())
+      .then((data) => {
+        const categories = [...new Set(data.map((quiz) => quiz.category))];
+        setQuizCategory(categories);
+      });
+  }, []);
+
   return (
-    <div>HomePage</div>
+    <div className="HomePage">
+      <div className="content-container">
+        <div className="welcome-message">
+          <h2>Welcome to Quiz Generator</h2>
+        </div>
+        <div className="body">
+          <div className="user-score">
+            <h4>Quiz Score</h4>
+          </div>
+          <div className="categories">
+            <h4>Quiz Categories</h4>
+            <ul className="category-items">
+              {quizCategory.map((category) => (
+                <li className="list-items" key={category}>{category}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
