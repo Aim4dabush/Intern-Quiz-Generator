76763726ec55924450eef07978dce8fe14c7a12e
Kimberly Encinas <kimbencinas@gmail.com>
2023-10-16 16:33:29 -0700

    basics done

--- src/pages/finish-page/FinishPage.jsx
+++ src/pages/finish-page/FinishPage.jsx
@@ -1,8 +1,18 @@
+import React from 'react';
 import "./FinishPage.css";
 
 function FinishPage() {
+
   return (
-    <div>FinishPage</div>
+    <div className="FinishPage">
+      <div className="content-container">
+        <h2>Quiz Results</h2>
+        <p>Score:</p>
+        <p>You Got:</p>
+        <Button onClick>Replay</Button>
+        <Button onClick>Back</Button>
+      </div>
+    </div>
   )
 }
 
