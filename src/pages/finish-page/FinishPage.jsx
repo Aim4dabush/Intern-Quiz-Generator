e233148506f5cb38f297ce5e977539bd26563d64
Kimberly Encinas <kimbencinas@gmail.com>
2023-10-18 18:22:11 -0700

    commit

--- src/pages/finish-page/FinishPage.jsx
+++ src/pages/finish-page/FinishPage.jsx
@@ -6,11 +6,17 @@ function FinishPage() {
   return (
     <div className="FinishPage">
       <div className="content-container">
-        <h2>Quiz Results</h2>
-        <p>Score:</p>
-        <p>You Got:</p>
-        <Button onClick>Replay</Button>
-        <Button onClick>Back</Button>
+        <div className="header">
+          <h2>Quiz Results</h2>
+        </div>
+        <div className="body">
+          <div className="score">
+            <p>Score:</p>
+          </div>
+          <div className="result">
+            <p>You Got:</p>
+          </div>
+        </div>
       </div>
     </div>
   )
