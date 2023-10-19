1f7fa15caf65eef4f420fd694472baea0b80a7a9
Gome510 <bluehawkfire55@gmail.com>
2023-10-18 17:45:24 -0700

    Remove My Finish Page
    prevent conflict w/ Kimberly finish page

--- src/pages/finish-page/FinishPage.jsx
+++ src/pages/finish-page/FinishPage.jsx
@@ -1,39 +1,7 @@
-import { Link, useLocation } from "react-router-dom";
 import "./FinishPage.css";
-import { Button } from "react-bootstrap";
 
 function FinishPage() {
-  let { state } = useLocation();
-  console.log(state);
-
-  const background = {
-    backgroundImage: 'url("/assets/images/cool-background.png")',
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-  };
-
-  return (
-    <div className="v-h-center h-100 p-3" style={background}>
-      <div className="w-75 w-md-50 h-50 p-2 bg-light d-flex flex-column justify-content-around  rounded text-center">
-        <h1>
-          <span
-            className="d-inline-block align-middle"
-            style={{ lineHeight: "normal" }}
-          >
-            Your Score: {state.score}/10
-          </span>
-        </h1>
-        <div className="d-flex justify-content-around ">
-          <Link to={"/prepare"}>
-            <Button size="lg">Try Again</Button>
-          </Link>
-          <Link to={"/"}>
-            <Button size="lg">Home</Button>
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+  return <div>FinishPage</div>;
 }
 
 export default FinishPage;
